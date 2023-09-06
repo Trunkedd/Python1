b8f3bb8dec7118fc7e6bc78a9dd049eff6bf87ac
Trunkedd <Jahn12jensen@gmail.com>
2023-09-06 09:13:15 +0200

    Update main
    la til adventure room type thing

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+hp = 100
 
-# lag et program som spør om navn og alder 
-# og deretter skriver det ut i en setning med bare 1 print
-navn = input("Hva heter du? -> ")
-alder = input("Hvor gammel er du? -> ")
- 
-print("Hei " + navn + ", så hyggelig å snakke med deg. Du er", alder, "år")
- 
-# gjør om teksten alder til int
-alder = int(alder)
- 
-# hvis vi er over 18 år skal vi få en beskjed om det
- 
-if alder >= 18:
-    print("Du er over 18")
- 
-# om 100 år er jeg :
-print("Om 100 år er du ", alder + 100, " år.")
+def rom_1():
+    print("du er i et rom og du må enten")
+    valg = input("Velge rom 2 med A,eller rom 2 med B")
+    if valg =="A":
+        rom_2()# starter rom 2
+        if valg =="B":
+            rom_3()# start rom 3
+
+
+def rom_2():
+    global hp
+    print("Her var det en løve som angriper deg")
+    hp -= 90
+    print("Du har nå", hp,"liv igjen")
+
+    if hp <= 0:
+        game_over()
+
+    rom_1()
+
+def rom_3():
+    print("du er i rom 3 så du vant ikke sant eller vil du tilbake til rom 1")
+
+def game_over():
+    print("du døde")
+    exit()
+
+rom_1()
