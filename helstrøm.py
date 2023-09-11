fec86f30e7aacaaf94002fed7011a5f19e3df70e
Trunkedd <Jahn12jensen@gmail.com>
2023-09-11 08:20:37 +0200

    Hellstrøm
    liten update ikke ferdig

--- helstrøm.py
+++ helstrøm.py
@@ -1,10 +1,26 @@
+def hus2():
+    print("Du er nå hjemme men plutselig PLING Hellstrøm har sendt deg en melding")
+    Hellstrømhusetditt = input("Hei du Kan jeg komme på besøk til deg")
+    if nummeret 
+
+def ja1():
+    print("Du gå nummeret ditt til Hellstrøm og dro hjem etter det")
+    hus2
+
 def besvim():
-    "du besvimer"
-def går1():
-    print("Du går til hellstrøm og dere snakker i noen minuter og han hade det bra så han spurte om nummere ditt ")
-    nummeret = input("Får han hellstrøm nummeret ditt eller nope")
+    print("du besvimer")
 def nei1():
-    print("du sier nei til han han sier at det går greit at")
+    print("du sier nei til han han sier at det går greit at du ikke gidde han nummeret han inviterer deg til og drikke noe du takker ja")
+
+def går1():
+    print("Du går til hellstrøm og dere snakker i noen minuter og han hadde det bra så han spurte om nummere ditt ")
+    nummeret = input("Får han hellstrøm nummeret ditt eller nope ")
+    if nummeret == "nope":
+        nei1()
+    if nummeret == "ja":
+        ja1
+
+
 def ikke1():
     print("Du tenker at du burde respekter Hellstrøms fest og la han være i fred")
     men = input("Han ser deg stire og går mot deg du blir stresset hva gjør du går du mot han eller går du vekk ")
@@ -20,8 +36,8 @@ def fester1():
     kjendis = input("er det selveste Hellstrøm går du og snakker med Hellstrøm eller ikke ")
     if kjendis == "ikke":
         ikke1()
-    if kjendis == "snakker":
-        går1
+    if kjendis == "går":
+        går1()
 
 
 def start():
